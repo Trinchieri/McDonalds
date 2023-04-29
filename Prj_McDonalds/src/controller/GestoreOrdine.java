fcd1e6652c557ea252c690b8c3206cacb4c7bd57
Trinchieri Giada <trinchierig@pascal.local>
2023-04-29 08:47:50 +0200

    ADD: GestoreOrdini

--- Prj_McDonalds/src/controller/GestoreOrdine.java
+++ Prj_McDonalds/src/controller/GestoreOrdine.java
@@ -1,6 +1,12 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import model.HappyMeal;
+import model.McCafe;
 import model.Ordine;
 
 public class GestoreOrdine {
@@ -10,5 +16,35 @@ public class GestoreOrdine {
         if (ordine == null) throw new Exception ("Devi inserire un ordine");
         
         ordini.add(ordine);
+    }
+    
+    /*public void inputOrdini () throws FileNotFoundException, IOException {
+        BufferedReader br = new BufferedReader (new FileReader("ordini.csv"));
+        String line;
+        
+        while ((line = br.readLine()) != null) {
+            String info[] = line.split(",");
+            
+            switch (info[0]) {
+                case "McCafe": {
+                    ordini.add(new McCafe (info[1], info[2]));
+                    break;
+                }
+                
+                case "HappyMeal": {
+                    ordini.add(new HappyMeal (info[1], info[2], info[3], info[4]));
+                    break;
+                }
+                
+                case "McMenu": {
+                    ordini.add(new McMenu (info[1], info[2], info[3]);
+                    break;
+                }        
+            }
+        }
+    }*/
+
+    public ArrayList<Ordine> getOrdini() {
+        return ordini;
     }
 }
